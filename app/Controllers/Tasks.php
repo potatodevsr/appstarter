c4f2462b3379bab7dd6bf68a03ac2adb09d9fb55
potatodevs <hoshiku1997@gmail.com>
2023-03-25 16:57:17 +0700

    codeIgniter-controllers-views-layouts

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Tasks extends BaseController
 {
     public function index()
     {
-        return view("Home/index.php");
+       return view("Tasks/index.php");
     }
 }
---
+++ app/Views/Home/index.php
@@ -0,0 +1,9 @@
+<?= $this->extend("layouts/default") ?>
+
+<?= $this->section("title") ?>Home<?= $this->endSection() ?>
+
+<?= $this->section("content") ?>
+
+<h1>Wellcome</h1>
+
+<?= $this->endSection() ?>
